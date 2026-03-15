37179e45dfebf38fe18ec6569af129d920d34107
Alexandr <Tishechkin1994@gmail.com>
2026-03-15 10:23:02 +0300

    1

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
-import { registerSW } from 'virtual:pwa-register'
-import { brandName } from './utils/brand'
+import { createApp } from "vue"
+import { registerSW } from "virtual:pwa-register"
+import App from "./App.vue"
+import router from "./router"
+import { ensureCsrfCookie } from "./services/api"
+import "./style.css"
+import { brandName } from "./utils/brand"
 
 registerSW({ immediate: true })
 document.title = `${brandName} - интернет-магазин часов`
+void ensureCsrfCookie().catch(() => undefined)
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount("#app")
